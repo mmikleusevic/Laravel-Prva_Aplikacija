181152a5cac42b884fa211d9f8092b5e957d485a
mmikleusevic <mikleusevicmarin@gmail.com>
2018-10-23 13:37:43 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/Http/Controllers/DashboardController.php
    #	resources/views/dashboard.blade.php
    #	routes/web.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -23,6 +24,37 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('dashboard');
+        $users = User::all();
+        return view('dashboard',compact('users'));
     }
+    public function assignRole(Request $request)
+    {
+        $user = User::where('email',$request['email'])->first();
+        $user->roles()->detach();
+        if($request['role_user']){
+            $user->roles()->attach(Role::where('name','User')->first());
+        }
+        if($request['role_admin']){
+            $user->roles()->attach(Role::where('name','Admin')->first());
+        }
+        return redirect('/dashboard');
+    }
+    public function storeUser()
+	{
+		$this->validate(request(), [
+
+			'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+
+		]);
+
+		auth()->user()->publish(
+    		new Post(request(['title','body','password']))
+    	);
+
+    	session()->flash('message', 'Your user has been added.');
+
+		return redirect('/dashboard');
+	}
 }
