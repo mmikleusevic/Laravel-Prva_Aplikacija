be4f77874dccf71d48c34e41a087b42ae6c256c2
mmikleusevic <mikleusevicmarin@gmail.com>
2018-12-05 04:02:11 +0100

    ajax role update

---
+++ UpdateController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Role;
+use App\Update;
+use Auth;
+use Illuminate\Http\Request;
+
+class UpdateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $user = Auth::user();
+        return view('mojView.update', compact('user'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Update  $update
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Update $update)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Update  $update
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Update $update)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Update  $update
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Update $update)
+    {
+        $id = Auth::id();
+        $user = User::find($id);
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = $request->input('password');
+        $user->last_name = $request->input('last_name');
+        $user->phone = $request->input('phone');
+        $user->save();
+        return redirect('/update')->with('success', 'User updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Update  $update
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Update $update)
+    {
+        //
+    }
+}
